fc77a0b4bbdd6f9eeb00256b3fd537b35982a725
Elton Viana <eltonvs@outlook.com>
2018-10-24 18:39:52 -0300

    Start Project
    - Add buefy, prettier and sass as project dependencies
    - Create a styles folder with some scss utils
    - Change page title and add some Open Sans font and fontawesome
    - Add flat UI colors

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(Buefy);
 
 new Vue({
   router,
